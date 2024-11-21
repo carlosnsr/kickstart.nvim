137af82fd465f59d0ac220668067d736a4c813eb
Carlos Rodrigues <carlos.rodrigues@ibotta.com>
2024-11-21 16:23:33 -0700

    Adds markdown-preview

--- init.lua
+++ init.lua
@@ -954,6 +954,16 @@ require('lazy').setup({
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
 
+  -- For live markdown previews
+  { -- install without yarn or npm
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = function()
+      vim.fn['mkdp#util#install']()
+    end,
+  },
+  --
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
