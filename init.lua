80f3fd3fe7a41fee4168950e8a801f8bc512cdea
Carlos Rodrigues <carlos.rodrigues@ibotta.com>
2024-12-03 10:44:27 -0700

    Enforce soft tabs and indentation

--- init.lua
+++ init.lua
@@ -102,6 +102,14 @@ vim.g.have_nerd_font = false
 vim.o.foldmethod = 'indent'
 vim.o.foldlevel = 10
 
+-- Indentation
+vim.opt.smartindent = true
+-- Use indentation without hard tabs (see: http://vim.wikia.com/wiki/Indenting_source_code)
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.shiftwidth = 2
+-- vim.opt.softtabstop = 2
+
 -- Make line numbers default
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
