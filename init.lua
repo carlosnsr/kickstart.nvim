445e40a36df1e3340609b632a847654dc61a217c
Carlos Rodrigues <carlos.rodrigues@ibotta.com>
2024-12-05 14:51:13 -0700

    Adds co-pilot integration

--- init.lua
+++ init.lua
@@ -274,6 +274,7 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  'github/copilot.vim', -- co-pilot integration
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
@@ -1032,3 +1033,8 @@ require('lazy').setup({
 
 -- load custom configurations
 require 'plugin_config.typescript-tools'
+
+vim.api.nvim_create_autocmd({ 'VimEnter' }, {
+  pattern = { '*' },
+  command = 'Copilot enable',
+})
