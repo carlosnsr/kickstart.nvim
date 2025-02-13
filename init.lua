f6c54ae3d423d4d54e2fece4ef4d07df7d293156
Carlos Rodrigues <carlos.all.mail@gmail.com>
2025-02-13 11:35:21 -0700

    Adds more AI integration

--- init.lua
+++ init.lua
@@ -275,6 +275,13 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   'github/copilot.vim', -- co-pilot integration
+  { -- further co-pilot/AI integration
+    'olimorris/codecompanion.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+    },
+  },
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
@@ -1050,3 +1057,14 @@ vim.api.nvim_create_autocmd({ 'VimEnter' }, {
   pattern = { '*' },
   command = 'Copilot enable',
 })
+
+require('codecompanion').setup {
+  strategies = {
+    chat = {
+      adapter = 'copilot',
+    },
+    inline = {
+      adapter = 'copilot',
+    },
+  },
+}
