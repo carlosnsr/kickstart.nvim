8f650a322a484bbcc498b131f1d361b4a0448504
Carlos Rodrigues <carlos.all.mail@gmail.com>
2025-08-15 19:23:31 -0600

    Backup options now use o instead of opt

--- init.lua
+++ init.lua
@@ -202,18 +202,18 @@ vim.o.confirm = true
 -- Set all swap-files to go into a central directory
 local home_path = vim.fn.expand '$HOME'
 local tmp_dir = home_path .. '/.config/nvim/tmp/'
-vim.opt.swapfile = true
-vim.opt.dir = tmp_dir .. 'swapfiles'
+vim.o.swapfile = true
+vim.o.dir = tmp_dir .. 'swapfiles'
 
 -- So that undos are persistent across buffers and sessions
-vim.opt.undofile = true
+vim.o.undofile = true
 --   Where to store the undo history (be sure to make this a directory)
-vim.opt.undodir = tmp_dir .. 'undo_histories'
+vim.o.undodir = tmp_dir .. 'undo_histories'
 --
 -- -- Set up backups
-vim.opt.backup = true
-vim.opt.backupdir = tmp_dir .. 'backups'
-vim.opt.writebackup = true
+vim.o.backup = true
+vim.o.backupdir = tmp_dir .. 'backups'
+vim.o.writebackup = true
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
